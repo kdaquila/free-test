3845ce7ca22474e2e01742746bf87067f9e52bda
Kenneth DAquila <kfd182@gmail.com>
2023-06-19 12:52:05 -0500

    Update library exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export { SimpleTestLoader } from "./loadingTests/simpleTestLoader.js";
 export { simpleResultsPrinter } from "./printing/simpleResultsPrinter.js";
 export { simpleTestRunner } from "./runningTests/simpleTestRunner.js";
 export { SimplePipelineRunner } from "./testingPipeline/simplePipelineRunner.js";
+export type { TestGroup } from "./types/testGroup.js";
+export type { SingleTest } from "./types/singleTest.js";
+export type { TestResult } from "./types/testResult.js";
